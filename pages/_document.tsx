15a7ed52e6d4fab03a95f086c75c444b075acac6
Odilov Sarvarbek <adiof.sarvar@gmail.com>
2025-07-09 05:27:56 +0900

    feat: meta tags integrations onn document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,27 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        <meta
+          name="keyword "
+          content={
+            "nestar, nestar.uz, devex.uz, devex mern, mern nestjs fullstack"
+          }
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее. | " +
+            "한국에서 언제 어디서나 부동산을 사고 팔 수 있습니다.한국에서 언제 어디서나 부동산을 사고 팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
